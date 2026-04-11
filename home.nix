ecac94e478f79b1026029192a09c93968b307aba
coins <lrav35@proton.me>
2026-04-11 13:23:02 -0400

    ssh

--- home.nix
+++ home.nix
@@ -3,6 +3,25 @@
   home.homeDirectory = "/home/coins";
   home.stateVersion = "25.11";
   programs.bash.enable = true;
+  programs.ssh = {
+    enable = true;
+    enableDefaultConfig = false;
+
+    matchBlocks = {
+      "*" = {
+	addKeysToAgent = "yes";
+      };
+      "github.com" = {
+        hostname = "github.com";
+        user = "git";
+        identityFile = "~/.ssh/xps_key";
+       };
+    };
+  };
+  services.ssh-agent = {
+    enable = true;
+    enableBashIntegration = true;
+  };
   
   home.packages = with pkgs; [
     git
