d7ed3900466d0cf1525f3ebeedfeefd85982d765
coins <lrav35@proton.me>
2026-04-11 14:13:26 -0400

    auto hypr start

--- home.nix
+++ home.nix
@@ -2,7 +2,6 @@
   home.username = "coins";
   home.homeDirectory = "/home/coins";
   home.stateVersion = "25.11";
-  programs.bash.enable = true;
   programs.ssh = {
     enable = true;
     enableDefaultConfig = false;
@@ -18,6 +17,33 @@
        };
     };
   };
+  programs.bash = {
+    enable = true;
+
+    shellAliases = {
+      rebuild = "sudo nixos-rebuild switch --flake ~/nixos-config#xps";
+      fullRebuild = "sudo nixos-rebuild switch --flake ~/nixos-config#xps && home-manager switch --flake ~/nixos-config/ -b backup";
+      homeRebuild = "home-manager switch --flake ~/nixos-config/ -b backup";
+    };
+
+    profileExtra = ''
+      ## Source .bashrc for aliases and interactive settings on login shells
+      if [ -f "$HOME/.bashrc" ]; then
+        . "$HOME/.bashrc"
+      fi
+
+      ## Start Hyprland with UWSM (adapted from your Arch setup) ###
+      if [ -z "$WAYLAND_DISPLAY" ] \
+         && [ "$(tty)" = "/dev/tty1" ] \
+         && [ -z "$SSH_CLIENT" ] \
+         && [ -z "$SSH_TTY" ] \
+         && uwsm check may-start; then
+
+        exec uwsm start hyprland-uwsm.desktop
+      fi
+    ''; 
+  };
+
   services.ssh-agent = {
     enable = true;
     enableBashIntegration = true;
--- system/configuration.nix
+++ system/configuration.nix
@@ -78,6 +78,26 @@
   #   enableSSHSupport = true;
   # };
 
+  
+  xdg.portal = {
+     enable = true;
+     wlr.enable = false;
+     xdgOpenUsePortal = false;
+     extraPortals = [
+       pkgs.xdg-desktop-portal-hyprland
+       pkgs.xdg-desktop-portal-gtk
+     ];
+  };
+
+  programs.hyprland = {
+    enable = true;
+    withUWSM = true;
+    xwayland = {
+      enable = true;
+    };
+    portalPackage = pkgs.xdg-desktop-portal-hyprland;
+  };
+
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
